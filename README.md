79da8c6eb5be99de8aa4bb0f70b56fd4eeb8feb5
Cathy Le <cathy347le@gmail.com>
2022-02-03 17:22:28 -0500

    Setup frontend with create-react-app and update public folder

---
+++ README.md
@@ -0,0 +1,3 @@
+# Made from Scratch
+
+#### An Asian-American bakery offering pastries that are damn delicious
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Made from Scratch</h1>
+    </div>
+  );
+}
+
+export default App;
