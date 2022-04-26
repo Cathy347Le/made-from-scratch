9ccd1c540617c11e97b3f2a1c7336044bd1ea90a
Cathy Le <cathy-le@pluralsight.com>
2022-04-26 12:50:41 -0400

    remove logic in routes into a controllers folder

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const asyncHandler = require('express-async-handler');
-const Product = require('../models/productModel');
+const {
+	getProducts,
+	getProductById,
+} = require('../controllers/productControllers');
 
-router.get('/', (req, res) => {
-  //empty object grabs all the data
-  Product.find({})
-    .then((products) => res.json(products))
-    // .catch((err) => console.log(err)); Instead of console.log the error, render the error message right on the browser
-    .catch((err) => res.json({ message: err.message }));
-});
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-router.get('/:id', (req, res) => {
-  Product.findById(req.params.id)
-    .then((product) =>
-      product
-        ? res.json(product)
-        : res.status(404).json({ message: 'Product not found' })
-    )
-    .catch((err) => res.json({ message: err.message }));
-});
+//CHAINING PROMISES
+// router.get('/', (req, res) => {
+// 	//empty object grabs all the data
+// 	Product.find({})
+// 		.then((products) => res.json(products))
+// 		// .catch((err) => console.log(err)); Instead of console.log the error, render the error message right on the browser
+// 		.catch((err) => res.json({ message: err.message }));
+// });
+
+// router.get('/:id', (req, res) => {
+// 	Product.findById(req.params.id)
+// 		.then((product) =>
+// 			product
+// 				? res.json(product)
+// 				: res.status(404).json({ message: 'Product not found' })
+// 		)
+// 		.catch((err) => res.json({ message: err.message }));
+// });
 
 //ASYNC AWAIT METHOD
 // router.get(
