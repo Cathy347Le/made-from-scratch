8cae71e5093fe1e500418f1e8c54f97d58d67cc4
Cathy Le <cathy-le@pluralsight.com>
2022-04-26 17:36:47 -0400

    update user controller to return if user is logged in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
 	{
@@ -25,6 +26,11 @@ const userSchema = mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+//For user login, creating a method called matchPassword that searches if the entered password matches the hashed password in the system.
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //How a model is created from the schema
 const User = mongoose.model('User', userSchema);
--- notes.md
+++ notes.md
@@ -223,3 +223,5 @@ export const authUser = asyncHandler(async (req, res) => {
 	res.send({ email, password });
 });
 ```
+
+- Setup user endpoint to search for an authenicated user (no token generated yet) and test on Postman
