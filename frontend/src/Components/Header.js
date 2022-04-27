ba7f5c4e81f5a40ce758a5af0bbc15e14fabb1b9
Cathy Le <cathy-le@pluralsight.com>
2022-04-27 00:08:20 -0400

    Prettier formatting

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -5,41 +5,41 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
-  return (
-    <Navbar bg="primary" variant="dark">
-      <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>
-            <img
-              alt=""
-              src={navlogo}
-              width="32"
-              height="32"
-              className="d-inline-block align-top"
-            />{' '}
-            Made from Scratch
-          </Navbar.Brand>
-        </LinkContainer>
-        <Nav className="me-auto">
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/our-story">
-            <Nav.Link>Our Story</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/faq">
-            <Nav.Link>FAQ</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <Nav className="ml-auto">
-          <LinkContainer to="/cart">
-            <Nav.Link>Cart</Nav.Link>
-          </LinkContainer>
-          <Nav.Link href="#out-story">Sign In</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar bg='primary' variant='dark'>
+			<Container>
+				<LinkContainer to='/'>
+					<Navbar.Brand>
+						<img
+							alt=''
+							src={navlogo}
+							width='32'
+							height='32'
+							className='d-inline-block align-top'
+						/>{' '}
+						Made from Scratch
+					</Navbar.Brand>
+				</LinkContainer>
+				<Nav className='me-auto'>
+					<LinkContainer to='/'>
+						<Nav.Link>Home</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to='/our-story'>
+						<Nav.Link>Our Story</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to='/faq'>
+						<Nav.Link>FAQ</Nav.Link>
+					</LinkContainer>
+				</Nav>
+				<Nav className='ml-auto'>
+					<LinkContainer to='/cart'>
+						<Nav.Link>Cart</Nav.Link>
+					</LinkContainer>
+					<Nav.Link href='/login'>Sign In</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
+	);
 };
 
 export default Header;
